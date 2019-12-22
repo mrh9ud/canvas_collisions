495798d4d3f868eb8c4538fa0ac2e815017f51ba
github username <github email address>
2019-12-22 13:39:09 -0500

    nothing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,122 +1,180 @@
 document.addEventListener("DOMContentLoaded", function(){
-    startGame()
-    // newOrExistingUser()
-    // fetchUsers()
+    // startGame()
+    fetchUsers()
+    newOrExistingUser()
     // addNamEventListner()
 })
 
 
-function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(100, 75, 50, 0, 360 * Math.PI, "green");
+
+function newOrExistingUser(){
+    // document.getElementById("login").innerHTML = ""
+    const body = document.querySelector("body")
+
+    // document.querySelector("canvas").style.display = "none"
+    const buttonDiv = document.createElement("div")
+    buttonDiv.id = "newOrExistingUserBtn"
+    body.append(buttonDiv)
+
+    const createNewUserButton = document.createElement("button")
+    createNewUserButton.innerText = "New User"
+    createNewUserButton.addEventListener("click", createNewUser)
+    const createExistingUserButton = document.createElement("button")
+
+    createExistingUserButton.innerText = "Existing User"
+    createExistingUserButton.addEventListener("click", loginExistingUser)
+
+    const div = document.getElementById("choose-user-btn")
+    buttonDiv.append(createNewUserButton)
+    buttonDiv.append(createExistingUserButton)
 
 }
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 800;
-        this.canvas.height = 500;
-        this.context = this.canvas.getContext("2d");
-        
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 5);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      }
+function fetchUsers(){
+    fetch("http://localhost:3000/users")
+    .then(res => res.json())
+    .then(json => console.log(json))
 }
 
-function component(x, y, r, sAngle, eAngle, color) {
-    // this.width = width;
-    // this.height = height;
-    // this.r = radius;
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.sAngle = sAngle;
-    this.eAngle = eAngle
-    this.update = function(){
-    ctx = myGameArea.context;
-    ctx.beginPath();
-    // debugger
-    ctx.fillStyle = color;
-    ctx.arc(this.x, this.y, this.r, this.sAngle, this.eAngle, ctx.fillStyle);
-    ctx.stroke()
-    ctx.fill()
-    }
-  }
+function createNewUser(user){
+    const body = document.querySelector("body")
 
+    const createH1 = document.createElement("h1")
+    createH1.id = "h1FontPosition"
+    createH1.innerText = "Create your name"
+    body.append(createH1)
+    const createCanvas = document.createElement("canvas")
+    createCanvas.id = "canvas"
+    body.append(createCanvas)
 
-function updateGameArea() {
-    myGameArea.clear();
-    // debugger
-    myGamePiece.x += 1;
-    myGamePiece.update();
+    document.querySelector("#newOrExistingUserBtn").innerHTML = ""
+
+    const createInput = document.createElement("input")
+    createInput.type = "text"
+    createCanvas.append(createInput)
+
+    body.append(createInput) 
 }
 
+function loginExistingUser(user){
+    const body = document.querySelector("body")
 
+    const createH1 = document.createElement("h1")
+    createH1.id = "h1FontPosition"
+    createH1.innerText = "Enter your name"
+    body.append(createH1)
+    const createCanvas = document.createElement("canvas")
+    createCanvas.id = "canvas"
+    body.append(createCanvas)
 
+    document.querySelector("#newOrExistingUserBtn").innerHTML = ""
 
+    const createInput = document.createElement("input")
+    createInput.type = "text"
+    createCanvas.append(createInput)
 
+    body.append(createInput) 
+}
 
-
-
-
-// function newOrExistingUser(){
-//     // document.getElementById("login").innerHTML = ""
-//     document.querySelector("canvas").style.display = "none"
-//     const createNewUserButton = document.createElement("button")
-//     createNewUserButton.innerText = "New User"
-//     createNewUserButton.addEventListener("click", createNewUser)
-//     const createExistingUserButton = document.createElement("button")
-//     createExistingUserButton.innerText = "Existing User"
-//     createExistingUserButton.addEventListener("click", loginExistingUser)
-//     const div = document.getElementById("choose-user-btn")
-//     div.append(createNewUserButton)
-//     div.append(createExistingUserButton)
-
+// function addNamEventListner(){
+//     const name = document.querySelector("#choose-user-btn")
+//     name.addEventListener("submit", checkUserStatus)
 // }
 
-// function fetchUsers(){
-//     fetch("http://localhost:3000/users")
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-// }
+// function checkUserStatus(user){
+//     if(user.currentTarget.username.value === )
 
-// // function addNamEventListner(){
-// //     const name = document.querySelector("#choose-user-btn")
-// //     name.addEventListener("submit", checkUserStatus)
-// // }
-
-// // function checkUserStatus(user){
-// //     if(user.currentTarget.username.value === )
-
-// //     post request to json
-// //     if json db has username
+//     post request to json
+//     if json db has username
     
-// // }
+// }
 
 
 
 
 
-// function createNewUser(user){
-//     document.getElementById("choose-user-btn").innerHTML = ""
-//     document.querySelector("canvas").style.display = ""
-//     const canvas = document.querySelector("#mc")
-//     const createInput = document.createElement("input")
-//     createInput.type = "text"
-//     createInput.style.color = "red"
-//     canvas.append(createInput)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function startGame() {
+//     myGameArea.start();
+//     myGamePiece = new component(100, 75, 50, 0, 360 * Math.PI, "/assets/pizza.png", "image");
 
 // }
 
- 
-
-// function loginExistingUser(user){
-//     document.getElementById("choose-user-btn").innerHTML = ""
-//     document.querySelector("canvas").style.display = ""
+// var myGameArea = {
+//     canvas : document.createElement("canvas"),
+//     start : function() {
+//         this.canvas.width = 800;
+//         this.canvas.height = 500;
+//         this.context = this.canvas.getContext("2d");
+        
+//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//         this.interval = setInterval(updateGameArea, 5);
+//     },
+//     clear : function() {
+//         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+//       }
 // }
+
+// function component(x, y, r, sAngle, eAngle, url, type) {
+//     // this.width = width;
+//     // this.height = height;
+//     // this.r = radius;
+//     this.type = type;
+//     if(type == "image") {
+//         this.image = new Image();
+//         this.image.src = url;
+//     }
+//     this.x = x;
+//     this.y = y;
+//     this.r = r;
+//     this.sAngle = sAngle;
+//     this.eAngle = eAngle
+//     this.update = function(){
+//     ctx = myGameArea.context;
+//     // if(type == "image"){
+//     // ctx.beginPath()
+//     // ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+//     // }
+//     // else
+//     // {
+//     //     ctx.beginPath();
+//     //     // debugger
+//     //     ctx.fillStyle = color;
+//     //     ctx.arc(this.x, this.y, this.r, this.sAngle, this.eAngle, ctx.fillStyle, this.type);
+//     //     ctx.stroke()
+//     //     ctx.fill()
+//     // }
+//     }
+//   }
+
+
+// function updateGameArea() {
+//     myGameArea.clear();
+//     // debugger
+//     myGamePiece.x += 1;
+//     myGamePiece.update();
+// }
+
+
+
+
+
+
+
 
 
